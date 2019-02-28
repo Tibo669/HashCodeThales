01d29983840e9b220d860ff72484602454e62b36
Tibo669 <thib669@hotmail.fr>
2019-02-28 19:14:03 +0100

    lecture

--- hashcode_2k9_vdurif.py
+++ hashcode_2k9_vdurif.py
@@ -28,13 +28,13 @@ class Slide:
 	
 # FUNCTIONS
 def score(slide_1, slide_2):
-	score = 0
+    score = 0
 
     common_tags = slide_1.tags
     tags_slide1_not_slide2 = set(slide_1.tags)
-    tags_slide2_not_slide1 = 
-	
-	return min()
+    tags_slide2_not_slide1 =
+
+    return min()
 
 # LOAD DATA
 path = "a_example"
@@ -44,9 +44,18 @@ path = "a_example"
 #path = "e_shiny_selfies"
 
 mode = "r"
-with open (path + ".in", mode) as reader:
-	all_input = (reader.readlines())
-	reader.close()
+photos_vertical_list = []
+photos_horizontal_list = []
+with open (path + ".txt", mode) as reader:
+    all_input = reader.readlines()
+    N = int(all_input[0])
+    for line in all_input[1:]:
+        line_splitted =  line.split(' ')
+        if Photo(line_splitted[0] == 'V':
+            photos_vertical_list.append(Photo(line_splitted[0], line_splitted[2:]))
+        else:
+            photos_horizontal_list.append(Photo(line_splitted[0], line_splitted[2:]))
+
 
 #configuration = all_input[0].split(' ')  # save the first line of the file containing the configuration as a list of words
 #print (configuration)
@@ -61,6 +70,6 @@ outFile = open(path + ".out", "w+")
 
 result = []
 for val in result:
-	outFile.write(repr(val) + "\n")
+    outFile.write(repr(val) + "\n")
 
 outFile.close()
